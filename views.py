a198e68ddf76b7ab80b70e9ba3c3e60def4aa45f
Aaron Whittington <AaronWhittington8284@gmail.com>
2024-10-17 09:18:43 -0700

    Created empty view

--- views.py
+++ views.py
@@ -1,1 +1,8 @@
+import tkinter as tk
+from tkinter import ttk
 
+class mainApp(tk.Tk):
+    def __inint__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
